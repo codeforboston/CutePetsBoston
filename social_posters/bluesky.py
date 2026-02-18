633ba29c44d814dadfb9e3bea788a185958bd618
Jonny Johannes <jjohannes@wayfair.com>
2026-02-17 20:24:08 -0500

    test the interfaces workflow

--- social_posters/bluesky.py
+++ social_posters/bluesky.py
@@ -10,10 +10,8 @@ from abstractions import Post, PostResult, SocialPoster
 class PosterBluesky(SocialPoster):
     def __init__(self):
         # Handle environment variable validation internally
-        self.username = (os.environ.get("BLUESKY_HANDLE") or
-                        os.environ.get("BLUESKY_TEST_HANDLE"))
-        self.password = (os.environ.get("BLUESKY_PASSWORD") or
-                        os.environ.get("BLUESKY_TEST_PASSWORD"))
+        self.username = os.environ.get("BLUESKY_TEST_HANDLE")
+        self.password = os.environ.get("BLUESKY_TEST_PASSWORD")
         self._access_token = None
         self._did = None  # Decentralized identifier from the Bluesky session.
         self._is_available = bool(self.username and self.password)
