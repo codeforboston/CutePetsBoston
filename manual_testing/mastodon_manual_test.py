98f08b5c695ae92a0c7b58a7c998ef41909473d4
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2026-05-07 09:37:20 -0400

    Fix cutting link out in certain posts by prioritizing adoption link

--- manual_testing/mastodon_manual_test.py
+++ manual_testing/mastodon_manual_test.py
@@ -1,13 +1,25 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from abstractions import Post
+from abstractions import Post, AdoptablePet
 from social_posters.mastodon import PosterMastodon
 
 
 
 def main():
     poster = PosterMastodon()
+    pet = AdoptablePet("Brian", 
+                        "Labrador Retriever", 
+                        "White Labrador", 
+                        "Quahog", 
+                        "I am a writer!"*500, 
+                        "http://www.davidgorman.com/4quartets/", 
+                        "https://static.wikia.nocookie.net/familyguy/images/c/c2/FamilyGuy_Single_BrianWriter_R7.jpg/revision/latest?cb=20230807152447",
+                        11, 
+                        "Male", 
+                        None, 
+                        None
+                    )
 
     if not poster.authenticate():
         print("Authentication failed!")
@@ -15,12 +27,12 @@ def main():
     
     print("Authenticated to Mastodon!")
 
-    post = Post(
-        text="Test post",
-        image_url="https://static.wikia.nocookie.net/familyguy/images/c/c2/FamilyGuy_Single_BrianWriter_R7.jpg/revision/latest?cb=20230807152447",
-        alt_text="Cute animal",
-        tags=["Test", "Mastodon"],
-    )
+    post = poster.format_post(pet)
+
+    target_url = "http://www.davidgorman.com/4quartets/"
+
+    if target_url not in post.text:
+        print("Adoption link not posted!")
 
     result = poster.publish(post)
 
