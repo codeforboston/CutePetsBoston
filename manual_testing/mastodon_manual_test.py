5758039a2b4b496c71e3553033f78c0135b9a04c
patrickZWY <154947644+patrickZWY@users.noreply.github.com>
2026-05-08 11:10:36 -0400

    more tests

--- manual_testing/mastodon_manual_test.py
+++ manual_testing/mastodon_manual_test.py
@@ -1,18 +1,16 @@
 import sys, os
+import time
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from abstractions import Post, AdoptablePet
 from social_posters.mastodon import PosterMastodon
 
-
-
-def main():
-    poster = PosterMastodon()
+def postExceed500WordLimitWithAdoptionLink():
     pet = AdoptablePet("Brian", 
                         "Labrador Retriever", 
                         "White Labrador", 
                         "Quahog", 
-                        "I am a writer!"*500, 
+                        "I am a writer! Post exceeds limit with adoption link"*1000, 
                         "http://www.davidgorman.com/4quartets/", 
                         "https://static.wikia.nocookie.net/familyguy/images/c/c2/FamilyGuy_Single_BrianWriter_R7.jpg/revision/latest?cb=20230807152447",
                         11, 
@@ -20,6 +18,63 @@ def main():
                         None, 
                         None
                     )
+    return pet
+
+def postExceed500WordLimitWithoutAdoptionLink():
+    pet = AdoptablePet("Vinny", 
+                        "Unknown", 
+                        "Unknown", 
+                        "Quahog", 
+                        "I am 1/16th cat! Post exceeds word limit without adoption link."*1000, 
+                        None, 
+                        "https://static.wikia.nocookie.net/familyguyfanon/images/e/ec/Vinny_Griffin.png/revision/latest?cb=20161129110103",
+                        None, 
+                        "Male", 
+                        None, 
+                        None
+                    )
+    return pet
+
+
+def postWithin500WordLimitWithAdoptionLink():
+    pet = AdoptablePet("Ernie", 
+                        "Chicken", 
+                        "Unknown", 
+                        "Quahog", 
+                        "cluck. Post within word limit with adoption link.", 
+                        "https://poets.org/poem/having-coke-you", 
+                        "https://static.wikia.nocookie.net/villains/images/2/2e/Giant_chicken_animation.png/revision/latest?cb=20220615120124",
+                        None, 
+                        "Male", 
+                        None, 
+                        None
+                    )
+    return pet
+
+def postWithin500WordLimitWithoutAdoptionLink():
+    pet = AdoptablePet("Pouncy", 
+                        "Cat", 
+                        "Unknown", 
+                        "Quahog", 
+                        "Meow. Post within 500 limit without adoption link", 
+                        None, 
+                        "https://static.wikia.nocookie.net/villains/images/7/76/Pouncey.webp/revision/latest?cb=20220403224856",
+                        None, 
+                        "Male", 
+                        None, 
+                        None
+                    )
+    return pet
+
+testingCases = [
+    postExceed500WordLimitWithAdoptionLink,
+    postExceed500WordLimitWithoutAdoptionLink,
+    postWithin500WordLimitWithAdoptionLink,
+    postWithin500WordLimitWithoutAdoptionLink
+]
+
+def main():
+    poster = PosterMastodon()
 
     if not poster.authenticate():
         print("Authentication failed!")
@@ -27,19 +82,22 @@ def main():
     
     print("Authenticated to Mastodon!")
 
-    post = poster.format_post(pet)
+    for pet in testingCases:
+        pet_instance = pet()
+        
+        post = poster.format_post(pet_instance)
+        target_url = pet_instance.adoption_url
+        if target_url and (target_url not in post.text):
+            print("Adoption link not posted!")
 
-    target_url = "http://www.davidgorman.com/4quartets/"
+        result = poster.publish(post)
 
-    if target_url not in post.text:
-        print("Adoption link not posted!")
+        if result.success:
+            print(f"Posted successfully! URL: {result.post_url}")
+        else:
+            print(f"Post failed: {result.error_message}")
+        time.sleep(1)
 
-    result = poster.publish(post)
-
-    if result.success:
-        print(f"Posted successfully! URL: {result.post_url}")
-    else:
-        print(f"Post failed: {result.error_message}")
 
 if __name__ == "__main__":
     main()
