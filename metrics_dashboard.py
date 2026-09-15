1afe826d6d945ca2af4ba713314fe8479bccad0e
Peter Garrity <peter1@garritys.org>
2026-09-15 19:57:42 -0400

    Update dashboard to use refactored read database and upload html output as artifact

--- metrics_dashboard.py
+++ metrics_dashboard.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import plotly.express as px
-from main import read_database
+from database import _read_database
 
 
 def dashboard(html_file_path="dashboard.html"):
     # 1. Fetch data
-    data = read_database()
+    data = _read_database(database_path="database.json")
 
     df_posts = pd.json_normalize(
         data["posts"],
